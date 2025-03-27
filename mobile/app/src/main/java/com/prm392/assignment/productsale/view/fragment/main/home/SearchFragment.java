dcc012784884ed7f3e47989b6f4795a7161575c4
TuanPham <tuanpntse173039@fpt.edu.vn>
2025-03-27 08:40:38 +0700

    update import cat log

--- mobile/app/src/main/java/com/prm392/assignment/productsale/view/fragment/main/home/SearchFragment.java
+++ mobile/app/src/main/java/com/prm392/assignment/productsale/view/fragment/main/home/SearchFragment.java
@@ -2,6 +2,7 @@ package com.prm392.assignment.productsale.view.fragment.main.home;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
